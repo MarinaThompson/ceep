0d61501b30c12b727720702d3cca464edb1c032e
Marina Leticia <mlbtf@discente.ifpe.edu.br>
2021-06-04 11:04:23 -0300

    add notas dinamicamente

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/formularioCadastro/index";
-import './assets/index.css';
-import './assets/App.css';
+import "./assets/index.css";
+import "./assets/App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <section className="conteudo">
-      <FormularioCadastro/>
-     <ListaDeNotas/>
-    </section>
+class App extends Component {
+  constructor() {
+    super();  
 
-  );
+    this.state = {
+      notas:[]
+    }; 
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novaArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novaArrayNotas 
+    }
+    this.setState(novoEstado)
+    
+  }
+
+  render() {   
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
--- src/components/ListaDeNotas/listaDeNotas.jsx
+++ src/components/ListaDeNotas/listaDeNotas.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import CardNota from "../CardNota";
-import "./estilo.css"; 
+import "./estilo.css";
 class ListaDeNotas extends Component {
   render() {
-    return(
+    return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudos").map( (categoria, index) => {
-          return(
+        {this.props.notas.map((nota, index) => {
+          return (
             <li className="lista-notas_item" key={index}>
-              <CardNota/>
+              <CardNota titulo={nota.titulo} texto={nota.texto} />
             </li>
           );
         })}
